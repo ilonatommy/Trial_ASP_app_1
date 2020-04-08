28978fc40f1d0577a837fd6ec52bbf9d3b5705d9
ilonatommy <ili.ili@interia.pl>
2020-04-08 14:34:17 +0200

    Basic application listing movies from db and allowing edition. Contains a bug: no Id autonumeration.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,28 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Trial_app_1.Models;
 
 namespace Trial_app_1.Controllers
 {
     public class HomeController : Controller
     {
+        private Movies_DBEntities _db = new Movies_DBEntities();
+        // GET: Home
         public ActionResult Index()
         {
+            return View(_db.Movies.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
             return View();
         }
 
-        public ActionResult About()
+        // GET: Home/Create
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude ="Id")] Movie movieToCreate)
         {
-            ViewBag.Message = "Your contact page.";
+            try
+            {
+                _db.Movies.Add(movieToCreate);
+                _db.SaveChanges();
 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var movieToEdit = _db.Movies.Find(id);
+            return View(movieToEdit);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Movie movieToEdit)
+        {
+            try
+            {
+                var originalMovie = _db.Movies.Find(movieToEdit.Id);
+                _db.Entry(originalMovie).CurrentValues.SetValues(movieToEdit);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
             return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trial_app_1.Models;
+
+namespace Trial_app_1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // Action result has to have a View of the same name to be displayed properly
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Title = "Naked Gun" };
+            return View(movie);
+        }
+    }
+}
